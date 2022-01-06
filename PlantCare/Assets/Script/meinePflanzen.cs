76f28353b45fdf69ad8c6e7813f3b1a57f029dbc
jacanaro <aaron_jacob4692@yahoo.de>
2022-01-06 15:38:28 +0100

    meinePflanzen
    skript und prefab

--- PlantCare/Assets/Script/meinePflanzen.cs
+++ PlantCare/Assets/Script/meinePflanzen.cs
@@ -4,14 +4,9 @@ using UnityEngine;
 
 public class meinePflanzen : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject meinePflanzenButtonPrefab;
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
         
     }
