fcc23aad2f0461aadb537b4f6038d2b18e67fdf2
Carolina <45121184+RealAgnetha@users.noreply.github.com>
2022-01-11 17:36:53 +0100

    logik wie man das mood popup im pflanzenprofil öffnet

---
+++ PlantCare/Assets/Script/profil.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class profil : MonoBehaviour
+{
+  public GameObject moodPopup;
+
+    public void setMood() {
+        
+        moodPopup.SetActive(true);
+
+    }
+}
